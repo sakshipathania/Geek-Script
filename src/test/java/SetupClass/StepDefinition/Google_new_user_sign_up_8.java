8fad1948955a0c05e9035891ee21b8e7042f4464
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2021-12-03 17:01:08 +0530

    Update Google_new_user_sign_up_8.java

--- src/test/java/SetupClass/StepDefinition/Google_new_user_sign_up_8.java
+++ src/test/java/SetupClass/StepDefinition/Google_new_user_sign_up_8.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.JavascriptExecutor;
-
+import org.openqa.selenium.NoSuchElementException;
 import SetupClass.SetupClass;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -20,6 +20,8 @@ public class Google_new_user_sign_up_8 extends SetupClass {
 	public void user_is_already_on_Home_Page_of_Geeks_Website_viii() throws Throwable {
 		
 		driver.get(AppURL);
+		driver.manage().deleteAllCookies();
+		Thread.sleep(4000);
 		driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
 		log.info("It's opening the website URL");
 		Thread.sleep(1000);
@@ -28,58 +30,67 @@ public class Google_new_user_sign_up_8 extends SetupClass {
 
 	@Then("^user navigates to google login page viii$")
 	public void user_navigates_to_google_login_page_viii() throws Throwable {
-		WebElement g_login_btn = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#google-signin-btn > i")));
-		 Thread.sleep(3000);
-		 g_login_btn.click();
-		 Thread.sleep(3000);
+		try {
+			WebElement fb_login_btn = wait
+					.until(ExpectedConditions.elementToBeClickable(By.xpath("//i[@class='icon fa fa-google']")));
+			Thread.sleep(3000);
+			fb_login_btn.click();
+			Thread.sleep(3000);
+		} catch (NoSuchElementException e1) {
+			
+		}
 	}
 
 	@Then("^user login to website viii$")
 	public void user_login_to_website_viii() throws Throwable {
 		
-      driver.manage().window().maximize();
+                driver.manage().window().maximize();
 		
-		  // Store the CurrentWindow for future reference
-		 // String handle = " ";
-		  String currentWindow = driver.getWindowHandle();
-		  String popupWindowHandle = null;
-		   
-		  // Switch To Popup Window
-		  
-		  for(String handle : driver.getWindowHandles()){
-		   if(!handle.equals(currentWindow)){
-		    
-		    popupWindowHandle = handle;
-		     driver.switchTo().window(popupWindowHandle);
-			   driver.manage().window().maximize();
-		   }
-		  }
-	       Thread.sleep(5000);
-	       
-	        WebElement g_email = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div/div[1]/div/div[1]/input")));
-		Thread.sleep(3000);
-               g_email.sendKeys("slidetech.qa@gmail.com");
-          Thread.sleep(3000);
-    
-    WebElement g_login_btn1=wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#identifierNext > div > button > span")));
-		Thread.sleep(3000);
-	       g_login_btn1.click();
-		
-		Thread.sleep(3000);
-    
-               WebElement g_pass = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input")));
-		Thread.sleep(3000);
-               g_pass.sendKeys("Himanshi@123");
-	       
-	       Thread.sleep(3000);
-	       WebElement g_login_btn2=wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#passwordNext > div > button > span")));
-		Thread.sleep(3000);
-	       g_login_btn2.click();
-		
-		Thread.sleep(3000);
-		  driver.switchTo().window(currentWindow);
-		
-		Thread.sleep(3000);
+		 // Store the CurrentWindow for future reference
+		// String handle = " ";
+		String currentWindow = driver.getWindowHandle();
+		String popupWindowHandle = null;
+
+		// Switch To Popup Window
+
+		for (String handle : driver.getWindowHandles()) {
+			if (!handle.equals(currentWindow)) {
+
+				popupWindowHandle = handle;
+				driver.switchTo().window(popupWindowHandle);
+				driver.manage().window().maximize();
+
+				Thread.sleep(5000);
+
+				WebElement g_email = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(
+						"/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div/div[1]/div/div[1]/input")));
+				Thread.sleep(3000);
+				g_email.sendKeys("slidetech.qa@gmail.com");
+				Thread.sleep(3000);
+
+				WebElement g_login_btn1 = wait.until(ExpectedConditions
+						.elementToBeClickable(By.cssSelector("#identifierNext > div > button > span")));
+				Thread.sleep(3000);
+				g_login_btn1.click();
+
+				Thread.sleep(3000);
+
+				WebElement g_pass = wait.until(ExpectedConditions.elementToBeClickable(By.xpath(
+						"/html/body/div[1]/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div[1]/div/form/span/section/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/div[1]/input")));
+				Thread.sleep(3000);
+				g_pass.sendKeys("Himanshi@123");
+
+				Thread.sleep(3000);
+				WebElement g_login_btn2 = wait.until(
+						ExpectedConditions.elementToBeClickable(By.cssSelector("#passwordNext > div > button > span")));
+				Thread.sleep(3000);
+				g_login_btn2.click();
+
+				Thread.sleep(3000);
+			}
+		}
+		driver.switchTo().window(currentWindow);
+		Thread.sleep(4000);
 	}
 
 	@Then("^user is redirected my dashboard page viii$")
@@ -92,20 +103,22 @@ public class Google_new_user_sign_up_8 extends SetupClass {
 		
 		driver.get("https://www.slidegeeks.com/free-downloads");
 		Thread.sleep(6000);
-		
-		WebElement Download= driver.findElement(By.xpath("/html/body/div[1]/div[3]/div/section[2]/div/div/div[1]/div/div[2]/div[1]/div/div/p/a/span"));
-		
-		//WebElement Download = (WebElement)js.executeScript("('a.btn-download')", "");
+
+		WebElement Download = driver.findElement(
+				By.xpath("/html/body/div[1]/div[3]/div/section[2]/div/div/div[1]/div/div[2]/div[1]/div/div/p/a/span"));
+
+		// WebElement Download = (WebElement)js.executeScript("('a.btn-download')", "");
 		Thread.sleep(3000);
-		js.executeScript("arguments[0].scrollIntoView();",Download);
+		js.executeScript("arguments[0].scrollIntoView();", Download);
 		Download.click();
 		Thread.sleep(3000);
-		WebElement download_btn = driver.findElement(By.xpath("//*[@class=\"btn-download pg-button pg-addtocart pg-green-background-btn\"]"));
-		 Thread.sleep(3000);
-		js.executeScript("arguments[0].scrollIntoView();",download_btn);
+		WebElement download_btn = driver.findElement(
+				By.xpath("//a[@class='btn-download pg-button pg-addtocart pg-green-background-btn downloadFreePrd']"));
 		Thread.sleep(3000);
-		 download_btn.click();
-		 Thread.sleep(3000);
+		js.executeScript("arguments[0].scrollIntoView();", download_btn);
+		Thread.sleep(3000);
+		download_btn.click();
+		Thread.sleep(3000);
 		 
 	    	}
 
@@ -119,8 +132,13 @@ public class Google_new_user_sign_up_8 extends SetupClass {
 				 Thread.sleep(3000);
 				 log.info("Hey, I am on Home page Again after Sign out");
 				 Thread.sleep(1000);*/
-		 WebElement Signout = driver.findElement(By.xpath("//a[@href ='/logout']"));
-		Thread.sleep(3000);
-		Signout.click();
+		try {
+			WebElement Signout = wait.until(ExpectedConditions.elementToBeClickable(By.linkText("LOGOUT")));
+			Thread.sleep(3000);
+			Signout.click();
+			System.out.println("Logout click successfully");
+		} catch (NoSuchElementException e) {
+
+		}
 	}
 }
